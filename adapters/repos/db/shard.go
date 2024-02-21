fc62f62310386a344834d2c5e76e30908721a179
Jeronimo <jeronimo.irazabal@gmail.com>
2024-02-21 12:24:17 -0300

    init hashbeater after getting in sync with local shard
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

--- adapters/repos/db/shard.go
+++ adapters/repos/db/shard.go
@@ -327,10 +327,6 @@ func NewShard(ctx context.Context, promMetrics *monitoring.PrometheusMetrics,
 		s.index.logger.Printf("Created shard %s in %s", s.ID(), time.Since(before))
 	}
 
-	if s.hashtree != nil {
-		s.initHashBeater()
-	}
-
 	return s, nil
 }
 
@@ -657,6 +653,8 @@ func (s *Shard) initLSMStore(ctx context.Context) error {
 }
 
 func (s *Shard) initHashTree(ctx context.Context) error {
+	s.hashBeaterCtx, s.hashBeaterCancelFunc = context.WithCancel(context.Background())
+
 	if err := os.MkdirAll(s.pathHashTree(), os.ModePerm); err != nil {
 		return err
 	}
@@ -706,6 +704,7 @@ func (s *Shard) initHashTree(ctx context.Context) error {
 
 	if s.hashtree != nil {
 		s.hashtreeInitialized.Store(true)
+		s.initHashBeater()
 		return nil
 	}
 
@@ -742,6 +741,7 @@ func (s *Shard) initHashTree(ctx context.Context) error {
 		}
 
 		s.hashtreeInitialized.Store(true)
+		s.initHashBeater()
 	}()
 
 	return nil
@@ -749,9 +749,6 @@ func (s *Shard) initHashTree(ctx context.Context) error {
 
 func (s *Shard) buildCompactHashTree() {
 	s.hashtree = hashtree.NewCompactHashTree(math.MaxUint64, 16)
-	if s.hashtree != nil {
-		return
-	}
 }
 
 func (s *Shard) buildMultiSegmentHashTree() {
@@ -821,7 +818,7 @@ func (s *Shard) closeHashTree() error {
 }
 
 func (s *Shard) HashTreeLevel(ctx context.Context, level int, discriminant *hashtree.Bitset) (digests []hashtree.Digest, err error) {
-	if s.hashtree == nil || !s.hashtreeInitialized.Load() {
+	if !s.hashtreeInitialized.Load() {
 		return nil, fmt.Errorf("hashtree was not initialized")
 	}
 
